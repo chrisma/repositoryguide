c5941f91aa718b3b9029d38077bee41dfd94b45b
Christoph Matthies <chrisma@users.noreply.github.com>
2022-01-29 18:04:19 -0500

    We're building our own html partial engine!

--- assets/js/load_partials.js
+++ assets/js/load_partials.js
@@ -3,7 +3,10 @@
     document.querySelectorAll('[data-insert]').forEach(
         async function(el, index, listObj) {
             const response = await fetch(el.dataset.insert);
-            const html = await response.text();
+            let html = await response.text();
+            // replace placeholders in partial
+            // 'data-insert-param' states what to replace, 'data-insert-val' what it will be replaced with
+            html = html.replaceAll('#{' + el.dataset.insertParam + '}', el.dataset.insertVal)
             el.innerHTML = html;
         }
     );
